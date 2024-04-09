35a830a41337ced1e7e6946ec27b77698b5238b8
GlendyT <glendytuyuc@gmail.com>
2024-04-08 21:09:38 -0600

    agregar componentes expense

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm";
 import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
 import ExpenseModal from "./components/ExpenseModal";
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
   const { state } = useBudget();
@@ -21,10 +22,11 @@ function App() {
       </div>
 
       {isValidBudget && (
-      <main className="max-w-3xl mx-auto py-10"> 
-       <ExpenseModal />       
-      </main>
-)}
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseList />
+          <ExpenseModal />
+        </main>
+      )}
     </>
   );
 }
